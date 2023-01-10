a5d187aeba0d4348d969f4224ddb90eef90f7840
Kyrie890514 <kyrie890514@163.com>
2023-01-10 17:49:14 +0800

    runtime - 实现初始化 element 主流程

--- example/helloWorld/App.js
+++ example/helloWorld/App.js
@@ -2,7 +2,16 @@ import { h } from '../../lib/mini-vue3.esm.js'
 
 export const App = {
 	render() {
-		return h('div', 'Hello, ' + this.msg)
+		return h(
+			'div',
+			{
+				id: 'root',
+				class: ['Kyrie', '890514']
+			},
+			// 'Hello, ' + this.msg
+			// 'Hello, world.'
+			[h('span', {}, 'Hello '), h('span', {}, 'world.')]
+		)
 	},
 	setup() {
 		return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -5,13 +6,35 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-	// TODO 
-	// processElement(vnode, container)
-	processComponent(vnode, container)
+	if (typeof vnode.type === 'string') {
+		processElement(vnode, container)
+	} else if (isObject(vnode.type)) {
+		processComponent(vnode, container)
+	}
 }
 
 function processElement(vnode, container) {
+	mountElement(vnode, container)
+}
+function mountElement(vnode, container) {
+	const { type, children, props } = vnode
+	const el = document.createElement(type)
+	if (typeof children === 'string') {
+		el.textContent = children
+	} else if (Array.isArray(children)) {
+		mountChildren(vnode, el)
+	}
+	for (const key in props) {
+		const val = props[key]
+		el.setAttribute(key, val)
+	}
+	container.append(el)
+}
 
+function mountChildren(vnode, container) {
+	vnode.children.forEach(vnode => {
+		patch(vnode, container)
+	})
 }
 
 function processComponent(vnode, container) {
