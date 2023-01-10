7dae639dd74c0670faf182ffd90778187942fa07
Kyrie890514 <kyrie890514@163.com>
2023-01-10 13:58:59 +0800

    reactivity - 实现 reactive 和 readonly 嵌套对象转换功能

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
 import { track, trigger } from "./effect"
-import { ReactiveFlags } from "./reactive"
+import { reactive, ReactiveFlags, readonly } from "./reactive"
+import { isObject } from "../shared"
 
 function createGetter(isReadonly = false) {
 	return function get(target, key) {
@@ -9,6 +10,9 @@ function createGetter(isReadonly = false) {
 			return isReadonly
 		}
 		const res = Reflect.get(target, key)
+		if (isObject(res)) {
+			return isReadonly ? readonly(res) : reactive(res)
+		}
 		!isReadonly && track(target, key)
 		return res
 	}
