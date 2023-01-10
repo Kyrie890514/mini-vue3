e588323d0b61ac4d28e9bc87e32c528c673a6a5e
Kyrie890514 <kyrie890514@163.com>
2023-01-10 10:39:11 +0800

    reactivity - 实现 effect 的 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,19 +1,24 @@
+import { extend } from "../shared"
+
 const targetMap = new Map()
 
 let activeEffect
 
 export function track(target, key) {
-	let depsMap = targetMap.get(target)
-	if (!depsMap) {
-		depsMap = new Map()
-		targetMap.set(target, depsMap)
+	if (activeEffect) {
+		let depsMap = targetMap.get(target)
+		if (!depsMap) {
+			depsMap = new Map()
+			targetMap.set(target, depsMap)
+		}
+		let dep = depsMap.get(key)
+		if (!dep) {
+			dep = new Set()
+			depsMap.set(key, dep)
+		}
+		dep.add(activeEffect)
+		activeEffect.deps.push(dep)
 	}
-	let dep = depsMap.get(key)
-	if (!dep) {
-		dep = new Set()
-		depsMap.set(key, dep)
-	}
-	dep.add(activeEffect)
 }
 
 export function trigger(target, key) {
@@ -30,6 +35,9 @@ export function trigger(target, key) {
 
 class ReactiveEffect {
 	private _fn: any
+	deps = []
+	active = true
+	onStop?: () => void
 	constructor(fn, public scheduler?) {
 		this._fn = fn
 	}
@@ -37,10 +45,30 @@ class ReactiveEffect {
 		activeEffect = this
 		return this._fn()
 	}
+	stop() {
+		if (this.active) {
+			cleanUpEffect(this)
+			this.onStop?.()
+			this.active = false
+		}
+	}
+}
+
+function cleanUpEffect(effect) {
+	effect.deps.forEach((dep: any) => {
+		dep.delete(effect)
+	})
 }
 
 export function effect(fn, options: any = {}) {
 	const _effect = new ReactiveEffect(fn, options.scheduler)
+	extend(_effect, options)
 	_effect.run()
-	return _effect.run.bind(_effect)
+	const runner: any = _effect.run.bind(_effect)
+	runner.effect = _effect
+	return runner
+}
+
+export function stop(runner) {
+	runner.effect.stop()
 }
