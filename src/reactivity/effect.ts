e91e0a0e7b04ad7c55f62aa5c60256da52e4984c
Kyrie890514 <kyrie890514@163.com>
2023-01-10 10:08:23 +0800

    reactivity - 实现 effect 返回 runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -31,11 +31,12 @@ class ReactiveEffect {
 	}
 	run() {
 		activeEffect = this
-		this._fn()
+		return this._fn()
 	}
 }
 
 export function effect(fn) {
 	const _effect = new ReactiveEffect(fn)
 	_effect.run()
+	return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,8 +1,8 @@
 import { reactive } from "../reactive"
 import { effect } from "../effect"
 
-describe("effect", () => {
-	it("happy path", () => {
+describe('effect', () => {
+	it('happy path', () => {
 		const user = reactive({ age: 10 })
 		let age: number
 		effect(() => {
@@ -11,5 +11,17 @@ describe("effect", () => {
 		expect(age!).toBe(11)
 		user.age++
 		expect(age!).toBe(12)
+	})
+
+	it('should return runner when call effect', () => {
+		let foo = 10
+		const runner = effect(() => {
+			foo++
+			return 'foo'
+		})
+		expect(foo).toBe(11)
+		const r = runner()
+		expect(r).toBe('foo')
+		expect(foo).toBe(12)
 	})
 })
