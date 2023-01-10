bd1bbaf001fe10c73c5f1c64e9c6010035e32350
Kyrie890514 <kyrie890514@163.com>
2023-01-10 10:17:05 +0800

    reactivity - 实现 effect.scheduler 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -20,13 +20,17 @@ export function trigger(target, key) {
 	let depsMap = targetMap.get(target)
 	let dep = depsMap.get(key)
 	for (const effect of dep) {
-		effect.run()
+		if (effect.scheduler) {
+			effect.scheduler()
+		} else {
+			effect.run()
+		}
 	}
 }
 
 class ReactiveEffect {
 	private _fn: any
-	constructor(fn) {
+	constructor(fn, public scheduler?) {
 		this._fn = fn
 	}
 	run() {
@@ -35,8 +39,8 @@ class ReactiveEffect {
 	}
 }
 
-export function effect(fn) {
-	const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+	const _effect = new ReactiveEffect(fn, options.scheduler)
 	_effect.run()
 	return _effect.run.bind(_effect)
 }
