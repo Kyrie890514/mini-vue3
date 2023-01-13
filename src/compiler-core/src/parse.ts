08a1584dbfc3c09c29b8cecdc72f0dc965d1ebe3
Kyrie890514 <kyrie890514@163.com>
2023-01-13 15:58:23 +0800

    compiler - 实现解析三种联合类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -7,29 +7,52 @@ const enum TagType {
 
 export function baseParse(content: string) {
 	const context = createParserContext(content)
-	return createRoot(parseChildren(context))
+	return createRoot(parseChildren(context, []))
 }
 
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
 	const nodes: any[] = []
-	let node
-	const q = context.source
-	if (q.startsWith('{{')) {
-		node = parseInterpolation(context)
-	} else if (q[0] === '<') {
-		if (/[a-z]/i.test(q[1])) {
-			node = parseElement(context)
+	while (!isEnd(context, ancestors)) {
+		let node
+		const q = context.source
+		if (q.startsWith('{{')) {
+			node = parseInterpolation(context)
+		} else if (q[0] === '<') {
+			if (/[a-z]/i.test(q[1])) {
+				node = parseElement(context, ancestors)
+			}
 		}
+		if (!node) {
+			node = parserText(context)
+		}
+		nodes.push(node)
 	}
-	if (!node) {
-		node = parserText(context)
-	}
-	nodes.push(node)
 	return nodes
 }
 
+function isEnd(context, ancestors) {
+	const s = context.source
+	if (s.startsWith('</')) {
+		for (let i = ancestors.length - 1; i >= 0; i--) {
+			const tag = ancestors[i].tag
+			if (startsWithEndTagOpen(s, tag)) {
+				return true
+			}
+		}
+	}
+	return !s
+}
+
 function parserText(context: any) {
-	const content = parseTextData(context, context.source.length)
+	const endTokens = ['{{', '<']
+	let endIndex = context.source.length
+	for (const token of endTokens) {
+		const index = context.source.indexOf(token)
+		if (index !== -1 && index < endIndex) {
+			endIndex = index
+		}
+	}
+	const content = parseTextData(context, endIndex)
 	return {
 		type: NodeTypes.TEXT,
 		content
@@ -42,10 +65,21 @@ function parseTextData(context: any, length) {
 	return content
 }
 
-function parseElement(context: any) {
-	const element = parseTag(context, TagType.Start)
-	parseTag(context, TagType.End)
+function parseElement(context: any, ancestors) {
+	const element: any = parseTag(context, TagType.Start)
+	ancestors.push(element)
+	element.children = parseChildren(context, ancestors)
+	ancestors.pop()
+	if (startsWithEndTagOpen(context.source, element.tag)) {
+		parseTag(context, TagType.End)
+	} else {
+		throw new Error(`lack end tag ${element.tag}`)
+	}
 	return element
+}
+
+function startsWithEndTagOpen(source, tag) {
+	return source.startsWith('</') && source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
 }
 
 function parseTag(context: any, type) {
@@ -67,8 +101,8 @@ function parseInterpolation(context) {
 	advanceBy(context, openDelimiter.length)
 	const rawContentLength = closeIndex - openDelimiter.length
 	const rawContent = parseTextData(context, rawContentLength)
+	advanceBy(context, closeDelimiter.length)
 	const content = rawContent.trim()
-	advanceBy(context, rawContentLength + closeDelimiter.length)
 
 	return {
 		type: NodeTypes.INTERPOLATION,
